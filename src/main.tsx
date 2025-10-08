722d6fe108f627a883e8307ed9fe839234dd9d0d
bygoovideo-prog <bygoovideo@gmail.com>
2025-10-08 18:43:57 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './router'
+import './styles.css';
+
 createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>)
