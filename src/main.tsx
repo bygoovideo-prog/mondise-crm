51bd807299f6a6173151248da4c57072d9096e1d
bygoovideo-prog <bygoovideo@gmail.com>
2025-10-08 20:56:16 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import './styles.css';
 import React, { useEffect, useMemo, useState } from 'react';
 import { createRoot } from 'react-dom/client';       // ⬅️ NUEVO
 import './styles.css';                               // ⬅️ NUEVO
